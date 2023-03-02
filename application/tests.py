cea4f75fa8a433f31b01afae741f52ac1ff9f96f
daria.ch.prozorro <daria.ch.prozorro@gmail.com>
2023-03-02 11:16:59 +0200

    hw_21

--- application/tests.py
+++ application/tests.py
@@ -2,7 +2,7 @@ import uuid
 from django.test import Client
 
 from django.test import TestCase
-from application.models import UserList, Shoppinglist
+from application.models import UserList, Shoppinglist, Item
 from django.contrib.auth.models import User
 # Create your tests here.
 
@@ -43,9 +43,9 @@ class UserTestCase(TestCase):
 
     def test_user_not_exsist(self):
         user = Client()
-        user.login(username=self.user_name1, password=self.user_password1)
-        response = user.post('/user/invate', {'email': 'user3@gmail.com'})
-        self.assertEqual(response.status_code, 200)
+        user.login(username=self.user_name2, password=self.user_password2)
+        response = user.post('/user/invate/', {'email': 'user3@gmail.com'})
+        self.assertEqual(response.status_code, 404)
 
 
 class UserRegister(TestCase):
@@ -53,7 +53,6 @@ class UserRegister(TestCase):
         user = Client()
         response = user.post('/user/register',
                              {'username': 'user_3', 'email': 'user3@gmail.com', 'password': 'passuser3'})
-        self.assertEqual(response.status_code, 302)
         user3 = User.objects.filter(username='user_3').first()
         self.assertIsNotNone(user3)
         user3_list = UserList.objects.filter(id=user3.id).first()
@@ -61,7 +60,6 @@ class UserRegister(TestCase):
 
         response = user.post('/user/register',
                              {'username': 'user_4', 'email': 'user4@gmail.com', 'password': 'passuser4'})
-        self.assertEqual(response.status_code, 302)
         user4 = User.objects.filter(username='user_4').first()
         self.assertIsNotNone(user4)
         user4_list = UserList.objects.filter(id=user4.id).first()
@@ -74,14 +72,14 @@ class BuyItem(TestCase):
     fixtures = ['buy_item_fixture.json']
 
     def test_buy_item(self):
-        shop_list = Shoppinglist(list_id='199f4b29-c369-4543-893b-913738076321').all()
-        self.assertEqual(len(shop_list), 1)
-
+        shop_list1 = Shoppinglist.objects.filter(list_id='199f4b29-c369-4543-893b-913738076321').first()
         user = Client()
         user.login(username='user_2', password='2222')
-        response = user.post('/shoppinglist/1/buy', {'id': 1})
+        response = user.post('/shoppinglist/<item_id>/buy', {'item': 9})
         self.assertEqual(response.status_code, 302)
 
-        shop_list2 = Shoppinglist(list_id='199f4b29-c369-4543-893b-913738076321').all()
-        self.assertEqual(len(shop_list2), 1)
-        self.assertEqual(shop_list2[0].status, 'bought')
+        shop_list2 = Shoppinglist.objects.filter(list_id='199f4b29-c369-4543-893b-913738076321').first()
+        self.assertEqual('bought', shop_list2.status)
+
+
+
